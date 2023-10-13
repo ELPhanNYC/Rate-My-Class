0870d401456ed7f4ff4f49c35d67233dc5c05144
Julian Elmasry <julian31186@gmail.com>
2023-10-13 00:59:53 -0400

    mongo connection

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask , render_template , request , make_response
+from flask_pymongo import PyMongo
+import secrets
+import hashlib
+import bcrypt
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = 'mongodb://root:examplepass@mongodb:27017/rate_my_class?authSource=admin'
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
