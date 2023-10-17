3c7e1178e6cc0270a106630bd6683da548adc661
elphan <starsoccerethan@gmail.com>
2023-10-17 16:57:24 -0400

    Revert to common Mongo setup.

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ import bcrypt
 app = Flask(__name__)
 #app.config["MONGO_URI"] = 'mongodb://root:examplepass@mongodb:27017/rate_my_class?authSource=admin'
 mongo = MongoClient('mongo') # PyMongo(app)
+db = mongo["rmc"]
+posts = db["posts"]
+users = db["users"]
+
 
 @app.route("/", methods = ['GET']) #front end updated!
 def index_page():
@@ -28,14 +32,7 @@ def rating_page():
     content = render_template('rating.html')
     resp = make_response(content)
     resp.headers['X-Content-Type-Options'] = 'nosniff'
-    return resp
-    """
-    print("Path hit!")
-    response = make_response("Moved Permanently", 301)
-    response.headers["Location"] = '/'
-    return response
-    """
-    
+    return resp   
 
 @app.route("/login_page", methods = ['GET'])
 def login_page():
@@ -44,12 +41,12 @@ def login_page():
     resp.headers['X-Content-Type-Options'] = 'nosniff'
     return resp
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST']) # Reconfigured to work with common Mongo layout
 def login():
     login = False
     user = request.form.get("username")
     pwd = request.form.get("password")
-    auth_obj = mongo.db.users.find_one({"username" : user})
+    auth_obj = users.find_one({"username" : user})
     if auth_obj != None:
         if bcrypt.checkpw(pwd.encode(), auth_obj["password"]):
             login = True  
@@ -57,7 +54,7 @@ def login():
         auth_token = secrets.token_urlsafe(16)
         hashed_token = hashlib.sha256(auth_token.encode())
         hashed_bytes = hashed_token.digest()
-        mongo.db.tokens.update_one({"username" : user }, {"$set": {"auth_token": hashed_bytes}}, upsert=True)
+        users.update_one({"username" : user }, {"$set": {"auth_token": hashed_bytes}}, upsert=True)
         response = make_response("Moved Permanently",301)
         response.headers["Location"] = '/'
         response.set_cookie('auth_token', auth_token, httponly=True, max_age=3600)
@@ -66,17 +63,21 @@ def login():
 
 @app.route("/rating", methods=['POST'])
 def rating():
+    # Temp Code - Checking if path is hit
     print("Path hit!")
     response = make_response("Moved Permanently", 301)
     response.headers["Location"] = '/'
     return response
+    # Temp Code
 
 @app.route("/register", methods=['POST'])
 def register():
+     # Temp Code - Checking if path is hit
     print("Path hit!")
     response = make_response("Moved Permanently", 301)
     response.headers["Location"] = '/login.html'
     return response
+    # Temp Code
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
