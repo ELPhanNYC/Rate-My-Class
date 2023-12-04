c2d4705bb1fb3d0240390c4e18c1cb7a01deb594
anht-11 <kyanhvn11@gmail.com>
2023-12-04 01:45:56 -0500

    add secure websocket

--- static/script.js
+++ static/script.js
@@ -46,7 +46,7 @@ function updatePostsTime(time_data) {
 
 function initWS() {
     // Establish a WebSocket connection with the server
-    socket = io.connect(`http://${domain}`, {transports: ['websocket']});
+    socket = io.connect(`https://${domain}`, {transports: ['websocket']});
     socket.on('connect', (message) => {
         setInterval(getTime,1000)
         console.log('WebSocket connection established');
